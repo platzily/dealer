e883399e6a9c28c452b66e02d2ee3ed3686a6000
Enrique Perez <enrike_084@hotmail.com>
2021-11-21 16:30:43 -0600

    chore(config): add dotenv capabilities

--- config/rabbitmq.go
+++ config/rabbitmq.go
@@ -3,6 +3,7 @@ package config
 import (
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/platzily/dealer/utils/constants"
 	log "github.com/sirupsen/logrus"
 )
@@ -20,6 +21,12 @@ func ReadRabbitMQConfig() *RabbitMQConfig {
 }
 
 func getEnvVariableAsString(name string) string {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 
 	envVar := os.Getenv(name)
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/platzily/dealer
 go 1.16
 
 require (
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/streadway/amqp v1.0.0 // indirect
 	go.mongodb.org/mongo-driver v1.7.2 // indirect
